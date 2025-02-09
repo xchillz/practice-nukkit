6dcaff61874d109b8c5608d730da46cdbf41c632
xchillz <147117006+xchillz@users.noreply.github.com>
2025-02-09 17:45:22 -0300

    style: remove static keywords

--- src/main/java/dev/xchillz/practice/profile/ProfileRegistry.java
+++ src/main/java/dev/xchillz/practice/profile/ProfileRegistry.java
@@ -9,26 +9,26 @@ import java.util.UUID;
 
 public final class ProfileRegistry {
 
-    private static final Map<UUID, Profile> profiles = new HashMap<>();
+    private final Map<UUID, Profile> profiles = new HashMap<>();
 
-    public static void registerProfile(@NonNull Player player) {
-        UUID uuid = ProfileRegistry.getPlayerUUID(player);
+    public void registerProfile(@NonNull Player player) {
+        UUID uuid = this.getPlayerUUID(player);
 
-        profiles.put(uuid, Profile
+        this.profiles.put(uuid, Profile
                 .builder()
                 .uuid(uuid)
                 .name(player.getName()).build());
     }
 
-    public static Profile fetchProfile(@NonNull Player player) {
-        return profiles.get(ProfileRegistry.getPlayerUUID(player));
+    public Profile fetchProfile(@NonNull Player player) {
+        return this.profiles.get(this.getPlayerUUID(player));
     }
 
-    public static void clearProfile(@NonNull Player player) {
-        profiles.remove(ProfileRegistry.getPlayerUUID(player));
+    public void clearProfile(@NonNull Player player) {
+        this.profiles.remove(this.getPlayerUUID(player));
     }
 
-    private static UUID getPlayerUUID(@NonNull Player player) {
+    private UUID getPlayerUUID(@NonNull Player player) {
         return UUID.nameUUIDFromBytes(player.getName().toLowerCase().getBytes());
     }
 }
